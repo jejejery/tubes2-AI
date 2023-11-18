8c4d431e1b4bb9c76dc169baac7bfcc36b25434b
jejejery <13521131@std.stei.itb.ac.id>
2023-11-19 02:51:26 +0700

    add: categorical predictor

--- lib/main/Likelihood.py
+++ lib/main/Likelihood.py
@@ -1,25 +1,28 @@
 
+import numpy as np
+
 """
 implementasi dari fungsi likelihood untuk melakukan perhitungan likelihood suatu data
 prerequisite: data sudah dilakukan preprocessing
 
 input:
-    X: pandas dataframe dari fitur
-    y: pandas dataframe dari target
-    fitur: nama kolom fitur yang ingin dihitung likelihoodnya
+    Xi: np.array 1 dimensi, X ke i
+    y: np.array kolom target, 1 dimensi
+    
     target: nama kolom target yang bersesuaian dengan kolom fitur
     tipe_fitur: tipe fitur yang ingin dihitung likelihoodnya ("categorical" atau "numerikal" atau "gaussian")
 
 """
 class Likelihood:
     
-    def __init__(self, X, y, fitur, target, tipe_fitur):
+    def __init__(self, Xi, y, tipe_fitur, alpha=1):
         try:
-            self.fitur = fitur
-            self.target = target
-            self.tipe_fitur = tipe_fitur
-            self.X = X
+            self.Xi = Xi
             self.y = y
+            if(len(self.Xi) != len(self.y)):
+                raise Exception("jumlah data Xi dan y tidak sama")
+            self.tipe_fitur = tipe_fitur
+            self.alpha = alpha
         except:
             print("error in init Likelihood")
             raise
@@ -30,7 +33,7 @@ class Likelihood:
 
     """
     input:
-        x: data yang ingin dihitung likelihoodnya
+        value: data yang ingin dihitung likelihoodnya -> array like
     
     output:
         vector likelihood dari data x sesuai dengan fitur yang ingin dihitung likelihoodnya
@@ -38,12 +41,46 @@ class Likelihood:
         vektor likelihood ini akan digunakan untuk melakukan perhitungan posterior probability
 
     """
-    def count_likelihood(self, x):
+    def count_likelihood(self, value):
         if self.tipe_fitur == "categorical":
-            return self.count_likelihood_categorical(x)
+            return self.count_likelihood_categorical(value)
         elif self.tipe_fitur == "numerikal":
-            return self.count_likelihood_numerikal(x)
+            return self.count_likelihood_numerikal(value)
         elif self.tipe_fitur == "gaussian":
-            return self.count_likelihood_gaussian(x)
+            return self.count_likelihood_gaussian(value)
         else:
             return None
+        
+
+    """
+    fungsi untuk menghitung likelihood dari data numerikal
+    input:
+        value: data yang ingin dihitung likelihoodnya : array like
+    output:
+        vektor likelihood dari data x sesuai dengan fitur yang ingin dihitung likelihoodnya
+        misal kolom target memiliki 3 nilai unik, maka vector likelihoodnya memiliki panjang 3
+        vektor likelihood ini akan digunakan untuk melakukan perhitungan posterior probability (dalam log probability)
+    """
+    def count_likelihood_categorical(self, value):
+        try:
+            unique_val = np.unique(self.y) # nilai unik dari kolom target
+            print(value)
+            likelihood = [] # vector likelihood
+            for uval in unique_val:
+                # menghitung jumlah data yang memiliki nilai val pada kolom target
+                loc = np.where(self.y == uval)[0]
+                Xi_val = self.Xi[loc]
+                Xi_unique = len(np.unique(self.Xi))
+                # menghitung likelihood value terhadap Xi_val (data yang memiliki nilai val pada kolom target)
+                # likelihood mungkin bernilai 0 jika tidak ada data yang memiliki nilai val pada kolom target
+                # oleh karena itu, gunakan laplace smoothing dengan alp
+                # ha = 1
+                numerator = np.array([np.sum(Xi_val == elem) for elem in value])
+                denominator = len(Xi_val) + self.alpha * Xi_unique
+                likelihood.append( numerator/ (denominator))
+            print("likelihood")
+            print(likelihood)
+            return np.log(likelihood).T
+        except:
+            print("error in count_likelihood_categorical")
+            raise
--- lib/main/NaiveBayes.py
+++ lib/main/NaiveBayes.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import lib.main.Likelihood as lk
+from lib.main.Likelihood import Likelihood
 
 class NaiveBayes:
     """
@@ -8,14 +8,12 @@ class NaiveBayes:
     """
 
     """
-    @param categorical_features: array-like, shape = [n_features]
-        Indices of categorical features. contoh ["nama_hewan", "jenis_hewan", "jumlah_kaki"]
+    @param categorical_features: array-like, shape = [n_features]. contoh np.array([0, 1, 2, 3, 4]) -> 0, 1, 2, 3, 4 merupakan lokasi dari fitur kategorikal pada data
         Apabila suatu fitur numerik yang memenuhi tresshold tertentu (misal 10 unique values) maka fitur tersebut dapat dianggap sebagai fitur kategorikal
-    @param gaussian_features: array-like, shape = [n_features]
-        Indices of gaussian features for continuous feature. contoh ["berat_hewan", "tinggi_hewan", "clock_speed"]
-    @param non_gaussian_features: array-like, shape = [n_features]
-        Indices of non gaussian features for continuous feature. contoh ["jumlah_prosessor", "jumlah_faskes"].
-        Fitur ini merupakan diskritisasi dari fitur yang seharusnya kontinu
+    @param gaussian_features: array-like, shape = [n_features]. contoh seperti di atas
+        Indices of gaussian features for continuous feature.
+    @param non_gaussian_features: array-like, shape = [n_features]. contoh seperti di atas
+        Indices of non gaussian features for continuous feature. 
     @param alpha: float, optional (default=1.0)
         Smoothing untuk kalkulasi likelihood
     @param prior_probs: array-like, shape = [n_classes]
@@ -44,10 +42,7 @@ class NaiveBayes:
         
         """
         Menunjukkan fitur mana saja yang merupakan fitur kategorikal, gaussian, dan non gaussian
-        contoh: 
-        - categorical_features = ["hewan", "nama_kota"]
-        - gaussian_features = ["berat_hewan", "tinggi_hewan"]
-        - non_gaussian_features = ["jumlah_prosessor", "jumlah_faskes"]
+        contoh jelas seperti di atas, sebuah np.array yang menunjukkan index dari fitur kategorikal, gaussian, dan non gaussian
         """
         self.categorical_features = categorical_features
         self.gaussian_features = gaussian_features
@@ -60,14 +55,14 @@ class NaiveBayes:
 
         prior_probs: array-like, shape = [n_classes]
             Prior probabilities -> merupakan probabilitas dari masing-masing nilai unik dari kolom target (dalam log probability)
-        likehoods: array-like of array-like, shape = [n_features]
+        likelihoods: array-like of array-like, shape = [n_features]
             Mendefinisikan OBJEK likelihoods yang telah didefinisikan pada class Likelihood
             akan dipanggil pada method "fit" untuk menginisiasi fungsi likelihood
-            akan digunakan sebagai komponen untuk menghitung posterior probability pada method "predict" -> akan mereturn vector likelihood
+            akan digunakan sebagai komponen untuk menghitung posterior probability pada method "predict" -> akan mereturn vector likelihood (dalam log probability)
         """
-        self.prior_probs = []
-        self.likehoods = []
-        self.posteriors = []
+        self.prior_probs = None
+        self.likelihoods = None
+        self.posteriors = None
 
 
 
@@ -77,34 +72,87 @@ class NaiveBayes:
 
 
     """
-    X: pandas dataframe kolom fitur
-    y: nama kolom target pada dataframe X (dalam bentuk pandas series)
+    X: np.array 2 dimensi, data yang ingin dilakukan fitting
+    y: np.array kolom target, 1 dimensi
     """
 
     def fit(self, X, y):
         print("fitting NaiveBayes")
+        #jika X masih berupa pandas dataframe, maka akan diubah menjadi numpy array
+        if isinstance(X, pd.DataFrame):
+            X = X.values
+        #jika y masih berupa pandas series, maka akan diubah menjadi numpy array
+        if isinstance(y, pd.Series):
+            y = y.values
 
         #1. Kalkulasi prior probability
-        if(self.prior_probs is None):
-            self.prior_probs = self.count_prior_probs(y)
+   
+        self.prior_probs = self.count_prior_probs(y)
+        #2. build likelihoods
+        self.likelihoods = self.build_likelihoods(X, y)
+        self._is_fitted = True
 
+
+    """
+    X: np.array 2 dimensi, data yang ingin dilakukan fitting
+    y: np.array kolom target, 1 dimensi
+    """
+    def build_likelihoods(self, X, y):
+        
+        likelihoods = []
+        for i in range(X.shape[1]):
+            if i in self.categorical_features:
+                likelihoods.append(Likelihood(X[:, i], y, "categorical", self.alpha))
+            elif i in self.gaussian_features:
+                likelihoods.append(Likelihood(X[:, i], y, "gaussian", self.alpha))
+            elif i in self.non_gaussian_features:
+                likelihoods.append(Likelihood(X[:, i], y, "numerikal", self.alpha))
+            else:
+                print("error in build_likelihoods")
+                raise Exception("error in build_likelihoods")
+        return likelihoods
+    
+
+    """
+    X: np.array 2 dimensi, data yang ingin dilakukan fitting
+    """
+    def predict(self, X):
+        if isinstance(X, pd.DataFrame):
+            X = X.values
+        if not self._is_fitted:
+            raise Exception("model belum di fit")
+        else:
+            self.posteriors = self.posterior_probability(X)
+            return np.argmax(self.posteriors, axis=1)
+       
+        
+
+    def posterior_probability(self, X):
+        the_posteriors = None
+        for i in range(X.shape[1]):
+            if(i == 0):
+                the_posteriors = self.likelihoods[i].count_likelihood(X[:, i])
+            else:
+                the_posteriors = the_posteriors + self.likelihoods[i].count_likelihood(X[:, i])
+        the_posteriors = the_posteriors + self.prior_probs
+        return the_posteriors
 
     """
     y: pandas series dari kolom target
     """
     def count_prior_probs(self, y):
-        prior_probs = []
-        for target in y.unique():
-            prior_probs.append(y[y == target].count()/y.count())
-        return np.log(prior_probs)
+        log_prior_probs = []
+        unique_val = np.unique(y) # nilai unik dari kolom target
+        for uval in unique_val:
+            # menghitung jumlah data yang memiliki nilai val pada kolom target
+            loc = np.where(y == uval)[0]
+            log_prior_probs.append(len(loc)/len(y))
+        return np.log(log_prior_probs).squeeze()
     #will be implemented later
     """
     input:
         X: data yang ingin diprediksi, contoh X =<p1, p2, p3, p4, p5> , suatu vektor yang akan memprediksi nilai target Y berdasarkan fit
         y: nilai prediksi berdasarkan fitur X, contoh: y = 1 atau y = 0 (kasus binary classification)
     """
-    def predict(self, X):
-        return None
-
     def debug():
         print("debugging NaiveBayes module")
